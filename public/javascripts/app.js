36f4b0b173aff5aa9455124f9ca729413b7b5266
cara.zhou@uilanetworks.com <cara.zhou@uilanetworks.com>
2014-10-07 20:26:50 +0800

    With ExpressJS, so that I have "backend", I can start it from http way.

---
+++ public/javascripts/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var et = {};
+et.modules = {};
+
+/* App Module */
+
+et.modules.mainApp = angular.module('mainApp', ['ngRoute', 'et.modules.controllers']);
+
+et.modules.mainApp.config(function($routeProvider) {
+  $routeProvider
+   .when('/backlog', {
+    templateUrl: '../htmls/views/backlog.html',
+	//template: '<div>back log view {{name}}</div>',
+    controller: 'backlogController'
+    }
+  )
+  .when('/todo', {
+    templateUrl: '../htmls/views/todo.html',
+	//template: '<div>todo view {{name}}</div>',
+    controller: 'todoController'
+  }).
+  otherwise({
+	redirectTo: '/backlog'
+  });
+});
+
+/* Controllers */
+
+et.modules.controllers = angular.module('et.modules.controllers', []);
